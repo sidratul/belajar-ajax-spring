958259c2f4973fdfeb14a16d29a5bee6590bdcc3
Muhammad Sidratul Muntaha <muhammad.mochi@gmail.com>
2013-11-15 20:35:21 +0700

    tambah depedency

--- src/main/java/latihan/sidratul/ajax/spring/dao/impl/ProdukDaoImpl.java
+++ src/main/java/latihan/sidratul/ajax/spring/dao/impl/ProdukDaoImpl.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihan.sidratul.ajax.spring.dao.impl;
 
-/**
- *
- * @author sidratul
- */
 public class ProdukDaoImpl {
     
 }
